1d24a9e1c4a5b9632a5d0e9f902274ea05d4a30e
unknown <everythingfree4200@gmail.com>
2025-07-27 13:30:23 +0530

    0033

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
 import { forwardRef, useEffect, useImperativeHandle } from 'react';
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import React, { useRef } from 'react';
 
 export default function Home({ onCreate, onJoin }) {
   const [name, setName] = useState('');
